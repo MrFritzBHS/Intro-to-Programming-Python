dccc60da5ca19be6190383a8e8a5aae3d7a75434
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-03-11 11:24:13 -0400

    Create main.py

---
+++ Unit-4/Project-Mad-Libs/main.py
@@ -0,0 +1,7 @@
+# TODO 1 - print a greeting message to the user
+
+
+# TODO 2 - collect user input for *each* word that needs to be collected
+
+
+# TODO 3 - use the variables in the original madlib text to print the madlib 
