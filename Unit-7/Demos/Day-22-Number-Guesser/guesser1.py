c76c926f1dff75f91f2c18e7620cfe7b21a138dd
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-04-03 09:13:37 -0400

    Create guesser1.py

---
+++ Unit-7/Demos/Day-22-Number-Guesser/guesser1.py
@@ -0,0 +1,5 @@
+'''
+Number guessing game version 1
+
+Create a program that stores a secret number, and asks a user to guess the secret number. They will keep guessing the number until they are correct.
+'''
